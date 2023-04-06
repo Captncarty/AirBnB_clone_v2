83e8e193f98ad2272b86b33c81319d75e3d382a0
Eceeomar <isiomaenuonye@gmail.com>
2023-04-06 07:33:23 -0700

    compressed file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,31 @@
 #!/usr/bin/python3
+"""A Fabric script to create an archive file"""
+
+# Imported the necessary modules
 from fabric.api import local
-from time import strftime
-from datetime import date
+from datetime import datetime
 
 
 def do_pack():
-    """ A script that generates archive the contents of web_static folder"""
+    """Created an archive file of the web_static directory and return its path"""
 
-    filename = strftime("%Y%m%d%H%M%S")
+    # Get the current timestamp and format it as a string
+    time_now = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    # Created the filename by concatenating the timestamp, "web_static_", and the file extension
+    file_path = "versions/web_static_{}.tgz".format(time_now)
+
     try:
+        # Created a directory called "versions" if it doesn't already exist
         local("mkdir -p versions")
-        local("tar -czvf versions/web_static_{}.tgz web_static/"
-              .format(filename))
 
-        return "versions/web_static_{}.tgz".format(filename)
+        # Created an archive file of the web_static directory using the "tar" command
+        local("tar -cvzf {} web_static/".format(file_path))
+
+        # Return the path to the archive file
+        return file_path
 
     except Exception as e:
+        # Return None if an error occurs
         return None
+
