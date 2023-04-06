835a4c3a2df1235ba95ae5804b31c3e2386dfea4
Eceeomar <isiomaenuonye@gmail.com>
2023-04-06 10:03:19 -0700

    do deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,56 +1,30 @@
 #!/usr/bin/python3
-"""Compress web static package
-"""
-from fabric.api import *
-from datetime import datetime
-from os import path
+"""a script to send an archive file to a remote server
+and decompress it"""
 
+from fabric.api import run, env, put
+import os.path
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['54.162.93.251', '100.25.3.37']
+env.key_filename = '~/.ssh/school'
 env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'
-
 
 def do_deploy(archive_path):
-        """Deploy web files to server
-        """
-        try:
-                if not (path.exists(archive_path)):
-                        return False
-
-                # upload archive
-                put(archive_path, '/tmp/')
-
-                # create target dir
-                timestamp = archive_path[-18:-4]
-                run('sudo mkdir -p /data/web_static/\
-releases/web_static_{}/'.format(timestamp))
-
-                # uncompress archive and delete .tgz
-                run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
-/data/web_static/releases/web_static_{}/'
-                    .format(timestamp, timestamp))
-
-                # remove archive
-                run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
-
-                # move contents into host web_static
-                run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
-/data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
-
-                # remove extraneous web_static dir
-                run('sudo rm -rf /data/web_static/releases/\
-web_static_{}/web_static'
-                    .format(timestamp))
-
-                # delete pre-existing sym link
-                run('sudo rm -rf /data/web_static/current')
-
-                # re-establish symbolic link
-                run('sudo ln -s /data/web_static/releases/\
-web_static_{}/ /data/web_static/current'.format(timestamp))
-        except:
-                return False
-
-        # return True on success
-        return True
+    """a function to deploy code and decompress it"""
+    
+    if not os.path.isfile(archive_path):
+        return False
+    compressed_file = archive_path.split("/")[-1]
+    no_extension = compressed_file.split(".")[0]
+    
+    try:
+       remote_path = "/data/web_static/releases/{}/".format(no_extension)
+       sym_link = "/data/web_static/current"
+       put(archive_path, "/tmp/")
+       run("sudo mkdir - p {}".format(remote_path))
+       run("sudo tar -xvzf /tmp/{} -C {}".format(compressed_file, remote_path))
+       run("sudo rm /tmp/{}".format(compressed_file))
+       run("sudo ln -sf {} {}".format(remote_path, sym_link))
+       return True
+    except Exception as e:
+       return False
