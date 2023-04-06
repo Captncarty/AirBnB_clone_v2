e9c61ca8e39e932f04da774e16d7528e9cc8348e
Eceeomar <isiomaenuonye@gmail.com>
2023-04-06 07:03:37 -0700

    prepared my server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
-# Bash script that sets up web servers for the deployment of web_static
-sudo apt-get update
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
+# a script to install and conf nginx
 
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
+# script installs nginx
+sudo apt-get update
+sudo apt-get install nginx -y
+
+#create repositories
 sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html>
+sudo mkdir -p /data/web_static/shared/
+
+#dummy html page created
+echo "<!DOCTYPE html>
+<html>
   <head>
   </head>
   <body>
@@ -18,10 +19,14 @@ sudo echo "<html>
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+#create a symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
+#change directory name
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+#set up the page to be served
+sudo sed -i '/server_name _;/a \ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-available/default
 
+#restart the server
 sudo service nginx restart
